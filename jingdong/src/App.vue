d57e712c32a4e55f0d081ddcbe9dad8246670e5c
poicc <2631315464@qq.com>
2022-04-25 15:23:25 +0800

    底部docker

---
+++ jingdong/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6b8;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe6af;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe713;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe78b;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "./style/index.scss";
+
+.docker {
+  display: flex;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 0.01rem solid #f1f1f1;
+  color: $content-fontcolor;
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    &--active {
+      color: $highlight-color;
+    }
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+  }
+  &__title {
+    font-size: 0.2rem;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
+}
+</style>
