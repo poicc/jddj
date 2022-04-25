d59e92db5696712e304ff1bd425a16dfd409412c
poicc <2631315464@qq.com>
2022-04-25 15:25:23 +0800

    九宫格区域

--- jingdong/src/App.vue
+++ jingdong/src/App.vue
@@ -13,6 +13,92 @@
       <span class="iconfont">&#xe741;</span>
       <span class="warpper__search__text">山姆会员商店优惠商品</span>
     </div>
+
+    <!-- icons九宫格区 -->
+    <div class="icons">
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/超市.381xxdf9k0s0.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">超市便利</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/菜市场.xmmq0ne672o.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">菜市场</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/水果店.5rpv53yr71w0.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">水果店</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/鲜花.1l9r58g3j5eo.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">鲜花绿植</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/医药健康.15z8pd10yedc.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">医药健康</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/家居.7jp1bei9yo00.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">家居时尚</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/蛋糕.233n2l0flls0.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">烘培蛋糕</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/签到.2en5q1dnw7wg.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">签到</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/大牌免运.77xfzzc9fv40.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">大牌免运</p>
+      </div>
+      <div class="icons__item">
+        <img
+          class="icons__item__img"
+          src="https://cdn.jsdelivr.net/gh/poicc/image@main/红包.66unzn28xa80.webp"
+          alt=""
+        />
+        <p class="icons__item__desc">红包套餐</p>
+      </div>
+    </div>
+
+    <div class="gap"></div>
   </div>
 
   <div class="docker">
@@ -83,6 +169,32 @@
       width: 100%;
     }
   }
+}
+
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 0.16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display: block;
+      width: 0.4rem;
+      height: 0.4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: 0.06rem 0 0.16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+
+.gap {
+  margin: 0 -0.18rem;
+  height: 0.1rem;
+  background: #f1f1f1;
 }
 
 .docker {
